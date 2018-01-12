9baf073970c01678c3977bb4a5e7c0e8ac6df0e0
Angel Perez <iAngel.p93@gmail.com>
2018-01-12 13:20:38 -0400

    Update Fish Shell config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,26 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
+# Set nano as default editor
+set -gx EDITOR nano
 
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "/home/aitbw/.config/omf"
+rvm default
 
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
-# Fish-specific settings
-set -g theme_color_scheme base16
-set -g theme_display_cmd_duration no
-
-# Adds 'node' to current PATH
+# Add N to current PATH
 set -gx N_PREFIX $HOME/n
 set -gx PATH $PATH $N_PREFIX/bin
 
-# Adds 'scala' to current PATH
-set -gx SCALA_HOME /usr/local/share/scala
-set -gx PATH $PATH $SCALA_HOME/bin
+# Add pyenv to current PATH
+set -gx PYENV_ROOT $HOME/.pyenv
+set -gx PATH $PATH $PYENV_ROOT/bin
 
-rvm default
+# Add Rust to current PATH
+set -gx RUST_PREFIX $HOME/.cargo
+set -gx PATH $PATH $RUST_PREFIX/bin
+
+# Base16 shell
+# if status --is-interactive
+#  eval sh $HOME/.config/base16-shell/scripts/base16-seti.sh
+# end
+
+source ~/.config/fish/functions/aliases.fish
+source ~/.asdf/asdf.fish
+
+status --is-interactive; and source (pyenv init -|psub)
